fa89e8a6abb107412528e1d02e6831c91fb32608
dorigoa <alvise72@live.com>
2026-09-25 12:45:44 +0200

    Display THINK_BOOL value in launch script
    Added echo statement to display THINK_BOOL value.

--- launch-qwen3.8.sh
+++ launch-qwen3.8.sh
@@ -57,6 +57,9 @@ export HF_HUB_OFFLINE=1
 
 echo "Server launch: Model=$M, Port=$PORT, Thinking=$THINK_MODE"
 [[ $THINK_MODE -eq 1 ]] && THINK_BOOL="true" || THINK_BOOL="false"
+
+echo "thinkbool=$THINK_BOOL"
+
 mlx.launch --verbose --backend jaccl --hostfile ./hosts.json --env MLX_METAL_FAST_SYNCH=1 -- \
   $HOME/miniforge3/envs/mlx/bin/python -m mlx_lm.server \
   --model $M \
